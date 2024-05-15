277b5881ca4fc7e35704c719bf8a203c0e3232f7
Warabaringo <cris.herediabravo@alum.uca.es>
2024-05-15 19:54:17 +0200

    quitar_cursor + ganar

---
+++ Assets/Nuestro/Scripts/Ganar_puzle.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ganar_puzle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        // if(other.gameObject.tag == "Player")
+        // {
+            Debug.Log("Ganaste");
+            SceneManager.LoadScene("Principal");
+        // }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Ganaste");
+        SceneManager.LoadScene("Principal");
+    }
+
+}
