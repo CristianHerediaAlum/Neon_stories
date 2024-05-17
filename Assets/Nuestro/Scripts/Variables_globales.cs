01289c4c3ddb8e18297da767ed203c37ec2ed07b
CristianHerediaAlum <160254511+CristianHerediaAlum@users.noreply.github.com>
2024-05-17 17:58:31 +0200

    Update Variables_globales.cs

--- Assets/Nuestro/Scripts/Variables_globales.cs
+++ Assets/Nuestro/Scripts/Variables_globales.cs
@@ -6,6 +6,9 @@ public class Variables_globales : MonoBehaviour
 {
     public static int n_victorias = 0;
     public static Vector3 playerposition = new Vector3(426, 9, 915);
+    public static bool puzle_calamar = false;
+    public static bool puzle_tuberia = false;
+    public static bool puzle_jigsaw = false;
     // Start is called before the first frame update
     void Start()
     {
